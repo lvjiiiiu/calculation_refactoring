b3e5af5aa6ed1281b7915eb1e176345f0e3d8800
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-07 08:27:18 +0900

    リファクタリング

--- program_config.rb
+++ program_config.rb
@@ -1,37 +1,29 @@
 require "./unit_converter"
 
-  # 単位の設定(keyへ単位、valueへmgを1としたときの比)
-  # 単位は最も小さいものを1とし、昇順に並べる
-  def config_units
-    { "mg" => 1, "g" => 1000, "kg" => 1000000 }
-  end
+module ProgramConfig
 
-  module ProgramConfig
-
-  # 異常系の設定
-    def config_abnormal
-
+# 異常系の設定
+  def config_abnormal
     mass_exp = /[0-9]+[a-z]+/
     formula = @text.join(" ")
 
-    @text.each do |e|
-      unit = e.slice(/[a-z]+/)
+    # 引数の先頭が質量ではなかった場合
+    unless mass_exp.match?(@text.first)
+      raise ArgumentError.new("error: first argument must be weight-value(ex: 1kg)")
+    end
 
-      case
-      # 引数の先頭が質量ではなかった場合
-      when text[0] !~ mass_exp
-          raise ArgumentError.new("error: first argument must be weight-value(ex: 1kg)")
+    if /[*\/] [0-9]+[a-z]+/.match?(formula)
+      raise ArgumentError.new("error: multiplier & divisor must be a number at * , \/")
+    end
 
-      # 使用できない単位を使用した時
-      when (e =~ mass_exp) && !UnitConverter::UNITS.any? {|key, hash| unit == key }
-          raise ArgumentError.new("error: the unit must be one of #{UnitConverter::UNITS.keys}")
-
-      # 乗算演算、除算演算の乗数、除数に単位を指定した場合
-
-      when formula =~ /[*\/] [0-9]+[a-z]+/
-        raise ArgumentError.new("error: multiplier & divisor must be a number at * , \/")
+    @text.each do |token|
+      unit = token.slice(/[a-z]+/)
+      # もし、単位がなければ、続く処理をスキップ
+      next if unit.nill?
+      # 単位がついているとき、それがUNITSのkeyの中に含まれているか判定
+      unless UnitConverter::UNITS.keys.any?(unit)
+        raise ArgumentError.new("error: the unit must be one of #{UnitConverter::UNITS.keys}")
       end
-
     end
   end
 end
